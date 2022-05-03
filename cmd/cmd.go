cf2c21a69e6320a5440f66855022d3563f2277cc
Daniel S <49311112+periaate@users.noreply.github.com>
2022-05-03 17:02:11 +0300

    Documentation

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,13 +1,18 @@
+// package cmd contains all bindings to message commands, slash commands, etc.
+// New commands are added by creating a file in this package, defining the functions, commands
+// and creating an init() function which should then add the functions and/or commands to these maps.
+// These maps are called in server/server.go.
 package cmd
 
 import "github.com/bwmarrin/discordgo"
 
-// These maps make it so that adding functions to the bot is done on a file by file basis
 var (
-	// Commands is an array of metadata for commands
+	// Commands is a map of metadata for slash commands
+	// metadata includes things like name, description, etc.
 	Commands = map[string]*discordgo.ApplicationCommand{}
-	// Handlers is an array of functions for commands
+	// Handlers is a map of functions for slash commands
 	Handlers = map[string]func(bot *discordgo.Session, i *discordgo.InteractionCreate){}
-	// MessageCreate is an array of functions for MessageCreate commands
+	// MessageCreate is a map of functions for MessageCreate functions
+	// MessageCreate refers to commands which are triggered by normal discord messages
 	MessageCreate = map[string]func(bot *discordgo.Session, m *discordgo.MessageCreate){}
 )
